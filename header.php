c42bcafd4636e4afff925b2e44fddb044eb71733
jl <jlf@bcsys.fr>
2017-09-21 21:20:37 +0200

    surp old img

--- header.php
+++ header.php
@@ -42,7 +42,7 @@ if (isset($_SESSION['nom'])){
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>RED APPLE</title>
+  <title>SIX.DAVE</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
